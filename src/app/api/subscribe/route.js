ce2ee3697299ac931450951ee12c2f71682603ac
Aloy Aditya Sen <aloysen951@gmail.com>
2025-03-20 23:38:12 +0530

    sending the updates -- no use of resend api

--- src/app/api/subscribe/route.js
+++ src/app/api/subscribe/route.js
@@ -1,27 +1,27 @@
-import { NextResponse } from 'next/server';
-import { EmailTemplate } from '@/src/components/Email/template';
-import { Resend } from 'resend';
-export const runtime = 'edge';
-export const dynamic = 'force-dynamic';
+// import { NextResponse } from 'next/server';
+// import { EmailTemplate } from '@/src/components/Email/template';
+// import { Resend } from 'resend';
+// export const runtime = 'edge';
+// export const dynamic = 'force-dynamic';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(
-    req,
-) {
-    const body = await req.json();
-    const response = resend.emails.send({
-        from: 'onboarding@resend.dev',
-        to: body?.email,
-        subject: 'You are successfully subscribed to our newsletter',
-        react: EmailTemplate({ firstName: body?.email }),
-      });
+// export async function POST(
+//     req,
+// ) {
+//     const body = await req.json();
+//     const response = resend.emails.send({
+//         from: 'onboarding@resend.dev',
+//         to: body?.email,
+//         subject: 'You are successfully subscribed to our newsletter',
+//         react: EmailTemplate({ firstName: body?.email }),
+//       });
 
-  if ((await response)?.data) {
-    const data = (await response)?.data;
-    return NextResponse.json(data);
-  }else{
-    const data = (await response)?.error;
-    return NextResponse.json(data);
-  }
-}
+//   if ((await response)?.data) {
+//     const data = (await response)?.data;
+//     return NextResponse.json(data);
+//   }else{
+//     const data = (await response)?.error;
+//     return NextResponse.json(data);
+//   }
+// }
