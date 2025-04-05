e3e6034b64f39ad24a58af907f9b0ce70d42eb86
Aloy Aditya Sen <aloysen951@gmail.com>
2025-04-05 23:21:23 +0530

    making miinor upgrades to content

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -3,19 +3,20 @@ const siteMetadata = {
     title: 'Radii Labs | Quantitative Research Holding Firm with actionable insights',
     author: 'Radii Lab',
     headerTitle: 'Quantitative Research Holding Firm with actionable insights',
-    description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
+    description: ' Radii Labs is a quantitative research holding firm that provides actionable insights to help businesses make informed decisions.',
+    about: 'Radii Labs is a quantitative research holding firm that provides actionable insights to help businesses make informed decisions. We specialize in data analysis, market research, and business intelligence to help our clients stay ahead of the competition.',
     language: 'en-us',
     theme: 'system', // system, dark or light
     siteUrl: 'https://www.radii.in', // your website URL
     siteLogo: '/logo.png',
-    socialBanner: '/social-banner.png', // add social banner in the public folder
+    socialBanner: '/social-banner2.jpg', // add social banner in the public folder
     email: 'sales4radii@gmail.com', 
     github: 'https://github.com/radiilab',
     twitter: 'https://twitter.com/LabRadii',
     facebook: 'https://www.facebook.com/radiilab',
     youtube: 'https://youtube.com/',
     linkedin: 'https://www.linkedin.com/company/radii-lab',
-    dribbble: 'https://www.dribbble.com',
+    // dribbble: 'https://www.dribbble.com',
     locale: 'en-US',
   }
   
