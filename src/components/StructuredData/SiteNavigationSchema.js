2ceeb5cb4907398bd4254de5cddd696b81801f35
Aloy Aditya Sen <aloysen951@gmail.com>
2026-03-14 13:03:32 +0530

    I have significantly upgraded the structured data implementation to use the "perfect" @graph approach.
    This new version includes:
    
    Organization Schema: Specifically identifying Radii Labs, your logo, and all your social media profiles.
    WebSite Schema: Defining the primary site entity.
    Enhanced Site Navigation: A structured list of all key entry points and services.
    This interconnected structure is much more likely to be detected by the Google Rich Results Test (under 'Organization' and 'WebSite') and is the most robust way to encourage Google to generate site links. Please deploy these changes and test again!

--- src/components/StructuredData/SiteNavigationSchema.js
+++ src/components/StructuredData/SiteNavigationSchema.js
@@ -2,75 +2,118 @@ import React from "react";
 import siteMetadata from "@/src/utils/siteMetaData";
 
 const SiteNavigationSchema = () => {
+  const { siteUrl, title, author, email, linkedin, twitter, facebook, github, siteLogo } = siteMetadata;
+
   const schema = {
     "@context": "https://schema.org",
-    "@type": "ItemList",
-    "itemListElement": [
+    "@graph": [
       {
-        "@type": "SiteNavigationElement",
-        "position": 1,
-        "name": "Home",
-        "url": `${siteMetadata.siteUrl}/`
+        "@type": "Organization",
+        "@id": `${siteUrl}/#organization`,
+        "name": author,
+        "url": siteUrl,
+        "logo": {
+          "@type": "ImageObject",
+          "url": `${siteUrl}${siteLogo}`,
+          "width": 192,
+          "height": 192
+        },
+        "contactPoint": [
+          {
+            "@type": "ContactPoint",
+            "email": email,
+            "contactType": "customer service"
+          }
+        ],
+        "sameAs": [
+          linkedin,
+          twitter,
+          facebook,
+          github
+        ].filter(Boolean)
       },
       {
-        "@type": "SiteNavigationElement",
-        "position": 2,
-        "name": "About",
-        "url": `${siteMetadata.siteUrl}/about`
+        "@type": "WebSite",
+        "@id": `${siteUrl}/#website`,
+        "url": siteUrl,
+        "name": title,
+        "description": siteMetadata.description,
+        "publisher": {
+          "@id": `${siteUrl}/#organization`
+        },
+        "inLanguage": siteMetadata.language
       },
       {
-        "@type": "SiteNavigationElement",
-        "position": 3,
-        "name": "Contact",
-        "url": `${siteMetadata.siteUrl}/contact`
-      },
-      {
-        "@type": "SiteNavigationElement",
-        "position": 4,
-        "name": "Blogs",
-        "url": `${siteMetadata.siteUrl}/blogs`
-      },
-      {
-        "@type": "SiteNavigationElement",
-        "position": 5,
-        "name": "Console",
-        "url": "https://console.radii.in/"
-      },
-      {
-        "@type": "SiteNavigationElement",
-        "position": 6,
-        "name": "Algo Trading Platform",
-        "url": `${siteMetadata.siteUrl}/algo-trading-platform-india`
-      },
-      {
-        "@type": "SiteNavigationElement",
-        "position": 7,
-        "name": "Forex Algo Execution",
-        "url": `${siteMetadata.siteUrl}/forex-algo-execution-india`
-      },
-      {
-        "@type": "SiteNavigationElement",
-        "position": 8,
-        "name": "Quant Research Services",
-        "url": `${siteMetadata.siteUrl}/quant-research-services-india`
-      },
-      {
-        "@type": "SiteNavigationElement",
-        "position": 9,
-        "name": "Risk Managed Trading Automation",
-        "url": `${siteMetadata.siteUrl}/risk-managed-trading-automation`
-      },
-      {
-        "@type": "SiteNavigationElement",
-        "position": 10,
-        "name": "Multi-broker order routing",
-        "url": `${siteMetadata.siteUrl}/multi-broker-order-routing`
-      },
-      {
-        "@type": "SiteNavigationElement",
-        "position": 11,
-        "name": "Tutorial",
-        "url": `${siteMetadata.siteUrl}/tutorial`
+        "@type": "ItemList",
+        "name": "Navigation Menu",
+        "itemListElement": [
+          {
+            "@type": "SiteNavigationElement",
+            "position": 1,
+            "name": "Home",
+            "url": `${siteUrl}/`
+          },
+          {
+            "@type": "SiteNavigationElement",
+            "position": 2,
+            "name": "About",
+            "url": `${siteUrl}/about`
+          },
+          {
+            "@type": "SiteNavigationElement",
+            "position": 3,
+            "name": "Contact",
+            "url": `${siteUrl}/contact`
+          },
+          {
+            "@type": "SiteNavigationElement",
+            "position": 4,
+            "name": "Blogs",
+            "url": `${siteUrl}/blogs`
+          },
+          {
+            "@type": "SiteNavigationElement",
+            "position": 5,
+            "name": "Console",
+            "url": "https://console.radii.in/"
+          },
+          {
+            "@type": "SiteNavigationElement",
+            "position": 6,
+            "name": "Algo Trading Platform",
+            "url": `${siteUrl}/algo-trading-platform-india`
+          },
+          {
+            "@type": "SiteNavigationElement",
+            "position": 7,
+            "name": "Forex Algo Execution",
+            "url": `${siteUrl}/forex-algo-execution-india`
+          },
+          {
+            "@type": "SiteNavigationElement",
+            "position": 8,
+            "name": "Quant Research Services",
+            "url": `${siteUrl}/quant-research-services-india`
+          },
+          {
+            "@type": "SiteNavigationElement",
+            "position": 9,
+            "name": "Risk Managed Trading Automation",
+            "url": `${siteUrl}/risk-managed-trading-automation`
+          },
+          {
+            "@type": "SiteNavigationElement",
+            "position": 10,
+            "name": "Multi-broker order routing",
+            "url": `${siteUrl}/multi-broker-order-routing`
+          },
+          {
+            "@type": "SiteNavigationElement",
+            "position": 11,
+            "name": "Tutorial",
+            "url": `${siteUrl}/tutorial`
+          }
+        ]
       }
     ]
   };
